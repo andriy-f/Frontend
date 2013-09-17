56a48447c4d6e3c23e289649a36bc3bf98812fc7
Andriy <AndreusAnd@gmail.com>
2013-09-18 00:49:31 +0300

    play with js

--- js/crockford-good-parts.js
+++ js/crockford-good-parts.js
@@ -1,4 +1,5 @@
 (function(){
+    // ==========================================================
     // Book
 
     // Throughout the book, a method method is used to define new methods.
@@ -8,6 +9,11 @@
         return this;
     };
 
+    // When youmake a new object, youcan select the object that should be its prototype.
+    // The mechanism that JavaScript provides to do this is messy and complex, but it can
+    // be significantly simplified. We will add a create method to the Object function. The
+    // create method creates a new object that uses an old object as its prototype. There
+    // will be much more about functions in the next chapter.
     if (typeof Object.create !== 'function') {
         Object.create = function (o) {
             var F = function () {};
@@ -16,13 +22,62 @@
         };
     }
 
-    var empty_object = {};
     var stooge = {
         "first-name": "Jerome",
         "last-name": "Howard"
     };
     var another_stooge = Object.create(stooge);
+    stooge.dao = {cs: 'localhost'};
     
+    // The prototype link has no effect on updating. When we make changes to an object,
+    // the object’s prototype is not touched:
+    another_stooge['first-name'] = 'Harry';
+    another_stooge['middle-name'] = 'Moses';
+    another_stooge.nickname = 'Moe';
+    
+    console.log('This shows how change of "stooge" object also changes "another_stooge" - prototyped from "stooge" (memo field)' );
+    stooge.memo = 'our memo';
+    console.log('stooge: ');
+    console.log(stooge);
+    console.log('another_stooge: ');
+    console.log(another_stooge);
+    
+    delete another_stooge.nickname;
+    //console.log(another_stooge);
+    
+    // Features
+    var status = stooge.status || "unknown"; // default value
+    var nex = stooge.deo && stooge.deo.cs; // guard against 'type error'
+    var ex = stooge.dao && stooge.dao.cs;
+    
+    console.log('nex: ' + nex + ' ex: ' + ex);
+    
+    var flight = {
+        airline: "Oceanic",
+        number: 815,
+        departure: {
+            IATA: "SYD",
+            time: "2004-09-22 14:55",
+            city: "Sydney"
+        },
+        arrival: {
+            IATA: "LAX",
+            time: "2004-09-23 10:42",
+            city: "Los Angeles"
+        }
+    };
+    // =================================
+    // My stuff
+    function MyObjecto(data) {
+        this.status = 'active';
+        this.data = data;
+        this[''] = 'empty string value';
+    }
+    
+    
+    var myObjecto1 = new MyObjecto();
+    //console.log(myObjecto1);
+    delete myObjecto1;
     
     jQuery(function($){
         $('input[name="throw-ex"]').click(function(){
