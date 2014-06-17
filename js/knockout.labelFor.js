cd63f2dacfaa74611ba24daea303f26727e93a9e
Andriy Fetsyuk <AndreusAnd@gmail.com>
2014-06-17 19:59:02 +0300

    Knockout validation sample

--- js/knockout.labelFor.js
+++ js/knockout.labelFor.js
@@ -1,10 +1,30 @@
 (function () {
+
+    function hasRequiredRule(rules) {
+        if (Object.prototype.toString.call(rules) !== '[object Array]') {
+            return false;
+        }
+
+        var len = rules.length;
+        for (var i = 0; i < len; i++) {
+            if (rules[i].rule === 'required' && rules[i].params === true) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     ko.bindingHandlers.labelFor = {
         init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
-            var value = valueAccessor();
-            var valueUnwrapped = ko.unwrap(value);
-            $(element).prepend('<span style="color: red">*</span>');
-            $(element).find('input').css({'border': '1px solid red'});
+            if (!valueAccessor().rules) {
+                return;
+            }
+
+            var rules = ko.unwrap(valueAccessor().rules);
+            if (hasRequiredRule(rules)) {
+                $(element).prepend('<span style="color: red">*</span>');
+            }
         },
         update: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
             // This will be called once when the binding is first applied to an element,
