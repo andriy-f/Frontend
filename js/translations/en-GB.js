76992d44c0d45a993363958e7294639090423029
Andriy Fetsyuk (Intellias) <afetsyuk@intellias.com>
2014-06-27 13:31:36 +0300

    Added translations demos

---
+++ js/translations/en-GB.js
@@ -0,0 +1,8 @@
+﻿(function() {
+    window.frontend = window.frontend || {};
+    window.frontend.translations = [];
+    var t = window.frontend.translations;
+
+    t['center'] = 'Centre';
+    t['fileUnnuploadable'] = 'File can\u0027t be uploaded';
+})();
