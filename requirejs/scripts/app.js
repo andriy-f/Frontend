b7950e4dbed5e5fc7ab706801c9d4c133b5e111d
Andriy Fetsyuk <AndreusAnd@gmail.com>
2014-10-14 01:44:02 +0300

    Added sample requireJS with jQuery and bootstrap

---
+++ requirejs/scripts/app.js
@@ -0,0 +1,31 @@
+﻿//requirejs.config({
+//    "baseUrl": "scripts/lib",
+//    "paths": {
+//        "app": "../app"
+//    },
+//    "shim": {
+//        "jquery.alpha": ["jquery"],
+//        "jquery.beta": ["jquery"]
+//    }
+//});
+
+// For any third party dependencies, like jQuery, place them in the lib folder.
+
+// Configure loading modules from the lib directory,
+// except for 'app' ones, which are in a sibling
+// directory.
+requirejs.config({
+    baseUrl: 'scripts/lib',
+    paths: {
+        app: '../app'
+    },
+    shim: {
+        'bootstrap': {
+            deps: ['jquery']
+        }
+    }
+});
+
+// Start loading the main app file. Put all of
+// your application logic in there.
+requirejs(['app/main']);
